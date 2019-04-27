dc4022a79a7789426bbad9abdb1bf10cd61f5b27
oujdiidriss <44104228+oujdiidriss@users.noreply.github.com>
2019-04-27 17:16:22 +0100

    change0

--- src/main/java/com/example/assoc/JpaSpringApplication.java
+++ src/main/java/com/example/assoc/JpaSpringApplication.java
@@ -20,7 +20,7 @@ public class JpaSpringApplication {
 	public static void main(String[] args) {
 		ApplicationContext ctx = SpringApplication.run(JpaSpringApplication.class, args);
 		ContactRepository contact = ctx.getBean(ContactRepository.class);
-		contact.save(new Contact("jb525214","25/25/2200","20/01/2001","hicham@gmail.com","agadir","oujdi",1,"cc.jpg","driss"));
+		contact.save(new Contact("jb525214","25/25/2200","20/01/2001","hicham@gmail.com","jadida","oujdi",1,"cc.jpg","driss"));
 		//contact.save(new Contact());
 		
 		//List<Personne> personnes = personneDAO.findByCle("%dan%");
