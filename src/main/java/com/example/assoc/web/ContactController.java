38368ea05f07662491b0855551ab27456ce70368
Boulguid Kaoutar <49752595+boukaoutar@users.noreply.github.com>
2019-05-06 20:39:24 +0000

    temp

--- src/main/java/com/example/assoc/web/ContactController.java
+++ src/main/java/com/example/assoc/web/ContactController.java
@@ -6,10 +6,70 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 
 public class ContactController {
-	@RequestMapping("/data")
+
+	@RequestMapping("/index.html")
 	public String index()
 	{
 		return "index";
 	}
+	
+	@RequestMapping("/form.html")
+	public String form()
+	{
+		return "form";
+	}
 
+	@RequestMapping("/form_advanced.html")
+	public String formAD()
+	{
+		return "form_advanced";
+	}
+	
+	@RequestMapping("/form_validation.html")
+	public String formVLD()
+	{
+		return "form_validation";
+	}
+	
+	@RequestMapping("/form_wizards.html")
+	public String formWIZARDS()
+	{
+		return "form_wizards";
+	}
+	
+	@RequestMapping("/form_upload.html")
+	public String formUPLOAD()
+	{
+		return "form_upload";
+	}
+	
+	@RequestMapping("/form_buttons.html")
+	public String formBUTTONS()
+	{
+		return "form_buttons";
+	}
+	
+	@RequestMapping("/tables.html")
+	public String tables()
+	{
+		return "tables";
+	}
+	
+	@RequestMapping("/tables_dynamic.html")
+	public String tablesD()
+	{
+		return "tables_dynamic";
+	}
+
+	@RequestMapping("/fixed_sidebar.html")
+	public String fixedS()
+	{
+		return "fixed_sidebar";
+	}
+	
+	@RequestMapping("/fixed_footer.html")
+	public String fixedF()
+	{
+		return "fixed_footer";
+	}
 }
